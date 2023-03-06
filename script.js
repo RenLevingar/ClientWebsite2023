70df4543b0730c3f8876e00c1a1dd911126244ae
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-03-06 15:37:14 -0700

    made some of the functionality for the buttons

---
+++ script.js
@@ -0,0 +1,39 @@
+// Get the modal
+var modal1 = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+   if (event.target == modal2) {
+      modal.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal3 = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+   if (event.target == modal3) {
+      modal.style.display = "none";
+   }
+}
+
+// Get the modal
+var modal4 = document.getElementById('id04');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+   if (event.target == modal4) {
+      modal.style.display = "none";
+   }
+}
